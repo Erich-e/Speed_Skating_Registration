ec2e3c137ac078445223a316f2bb9a43395a8bc6
Erich Elendt <erich@mercury.(none)>
2015-03-11 18:02:33 -0400

    Latest update.

---
+++ upload.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title> Registration Form Update</title>
+ </head>
+<body>
+<div align="center">
+	<h1>Toronto Speed Skating Club<h1>
+
+	<table width="80%"><tr><td>
+	This is the administration page for the Speed Skating Registration Form.
+	Here, you can upload a new registration form template, new pricing
+	information or a new text file for printing.<p/>
+
+	In order to upload these files, please enter the password you were given.</td></tr>
+	</table> <table>
+	<form action = "upload.php" method = "post">
+	<tr><td>Format: </td><td> <input type = "file" name = "Format"></td></tr>
+	<tr><td>Amounts: </td><td> <input type = "file" name = "Amounts"></td></tr>
+	<tr><td>Price: </td><td> <input type = "file" name = "Price"></td></tr>
+	<tr><td>Tail: </td><td> <input type = "file" name = "Tail"></td></tr>
+	<tr><td>Password: </td><td> <input type = "password" name = "password"></td></tr>
+	
+	<form>
+	</td></tr></table>
+</div>
+</body>
+</html>
